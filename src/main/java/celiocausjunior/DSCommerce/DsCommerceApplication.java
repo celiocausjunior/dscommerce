cf81c937722df62ac8112046aa29af428db7c4bc
Celio Caus Junior <celiocausjunior@Mac-mini-de-Celio.local>
2023-08-30 22:15:54 -0300

    BCrypt Password

--- src/main/java/celiocausjunior/DSCommerce/DsCommerceApplication.java
+++ src/main/java/celiocausjunior/DSCommerce/DsCommerceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsCommerceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DsCommerceApplication.class, args);
 	}
